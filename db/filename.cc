f1cedae3458f67043beb2cd39bf8243ff1b2712f
Radim Kolar <hsn@sendmail.cz>
2013-01-30 22:50:41 +0100

    fix filename generation on windows

--- db/filename.cc
+++ db/filename.cc
@@ -18,9 +18,14 @@ extern Status WriteStringToFileSync(Env* env, const Slice& data,
 static std::string MakeFileName(const std::string& name, uint64_t number,
                                 const char* suffix) {
   char buf[100];
+#ifdef OS_WINDOWS
+  snprintf(buf, sizeof(buf), "/%06I64d.%s",
+           number, suffix);
+#else
   snprintf(buf, sizeof(buf), "/%06llu.%s",
            static_cast<unsigned long long>(number),
            suffix);
+#endif
   return name + buf;
 }
 
